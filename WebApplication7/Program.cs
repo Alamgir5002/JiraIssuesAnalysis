17fb2806c1791b46c1ceba9face310ec66fef6f7
Waqas Ahmed Rehmani <waqasahmed.rehmani@uworx.co.uk>
2024-03-27 19:56:04 +0500

    incorporated hangfire

--- WebApplication7/Program.cs
+++ WebApplication7/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Quartz;
 using WebApplication7.Models;
+using WebApplication7.Quartz.Jobs;
 using WebApplication7.Repository;
 using WebApplication7.Services;
 
@@ -20,10 +22,40 @@ builder.Services.AddScoped<HttpClientService>();
 builder.Services.AddScoped<SourceCredentialsRepository>();
 builder.Services.AddScoped<CustomFieldRepository>();
 
+// Schedule Quartz Jobs
+builder.Services.AddQuartz();
+builder.Services.AddQuartzHostedService(opts => opts.WaitForJobsToComplete = true);
+
+// Configure custom logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+//builder.Logging.AddDebug();
+
 string connectionString = builder.Configuration.GetConnectionString("SQLConnectionString") ?? throw new InvalidOperationException("Connection string of name SQLConnectionString not found");
 builder.Services.AddDbContext<DatabaseContext>(conn => conn.UseSqlServer(connectionString));
 
 var app = builder.Build();
+
+//// Schedule Quartz Jobs
+var schedulerFactory = app.Services.GetRequiredService<ISchedulerFactory>();
+var scheduler = await schedulerFactory.GetScheduler();
+
+// create a job and tie it to our JiraIssuesSyncJob class
+var jiraSyncJob = JobBuilder.Create<JiraIssuesSyncJob>()
+    .WithIdentity("JiraIssuesSyncJob", "JiraGroup")
+    .Build();
+
+// trigger the job to run now, and then every 40 seconds
+var trigger = TriggerBuilder.Create()
+    .WithIdentity("JiraIssuesSyncJobTrigger", "JiraGroup")
+    .StartNow()
+    .WithSimpleSchedule(x => x
+        .WithIntervalInSeconds(30)
+        .RepeatForever())
+    .Build();
+
+await scheduler.ScheduleJob(jiraSyncJob, trigger);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -38,4 +70,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
