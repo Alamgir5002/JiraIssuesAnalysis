ab1c02828afd543f7b4d220cd6ea1bfeb9168feb
Waqas Ahmed Rehmani <waqasahmed.rehmani@uworx.co.uk>
2024-03-28 12:53:05 +0500

    Removing Quartz from project

--- WebApplication7/Program.cs
+++ WebApplication7/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Quartz;
 using WebApplication7.Models;
-using WebApplication7.Quartz.Jobs;
 using WebApplication7.Repository;
 using WebApplication7.Services;
 
@@ -22,10 +20,6 @@ builder.Services.AddScoped<HttpClientService>();
 builder.Services.AddScoped<SourceCredentialsRepository>();
 builder.Services.AddScoped<CustomFieldRepository>();
 
-// Schedule Quartz Jobs
-builder.Services.AddQuartz();
-builder.Services.AddQuartzHostedService(opts => opts.WaitForJobsToComplete = true);
-
 // Configure custom logging
 builder.Logging.ClearProviders();
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
@@ -36,26 +30,6 @@ string connectionString = builder.Configuration.GetConnectionString("SQLConnecti
 builder.Services.AddDbContext<DatabaseContext>(conn => conn.UseSqlServer(connectionString));
 
 var app = builder.Build();
-
-//// Schedule Quartz Jobs
-var schedulerFactory = app.Services.GetRequiredService<ISchedulerFactory>();
-var scheduler = await schedulerFactory.GetScheduler();
-
-// create a job and tie it to our JiraIssuesSyncJob class
-var jiraSyncJob = JobBuilder.Create<JiraIssuesSyncJob>()
-    .WithIdentity("JiraIssuesSyncJob", "JiraGroup")
-    .Build();
-
-// trigger the job to run now, and then every 40 seconds
-var trigger = TriggerBuilder.Create()
-    .WithIdentity("JiraIssuesSyncJobTrigger", "JiraGroup")
-    .StartNow()
-    .WithSimpleSchedule(x => x
-        .WithIntervalInSeconds(30)
-        .RepeatForever())
-    .Build();
-
-await scheduler.ScheduleJob(jiraSyncJob, trigger);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
