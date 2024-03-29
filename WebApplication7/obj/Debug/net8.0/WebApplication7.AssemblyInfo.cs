72be9bc58d8538bf3ebebe79d7f28ad7066f5f1f
Alamgir Hassan <alamgir.hassan@uworx.co.uk>
2024-03-29 12:58:10 +0500

    Added merge remaining files

--- WebApplication7/obj/Debug/net8.0/WebApplication7.AssemblyInfo.cs
+++ WebApplication7/obj/Debug/net8.0/WebApplication7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187d5952095781ba7ebc1c0385dd29dc77fecdb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee3cb8888b8c05f1c02bb51915a52bce0a1f2c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
